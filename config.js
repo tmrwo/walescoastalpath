610d79d87434cff2293de663338e68300e1aed96
tmrwo <tmr.woodward@gmail.com>
2026-09-24 21:21:55 +0100

    Update OS Maps API key
    Updated OS Maps API key in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Paste your own keys here, save, and reload the site.
 const CONFIG = {
   // 1) OS Data Hub → your project → OS Maps API → "Project API Key"
-  osKey: "PASTE_YOUR_OS_MAPS_API_KEY_HERE",
+  osKey: "3Fr07wb3ygUU5iPBGWJFhv9iwpoTRNEP",
 
   // 2) Firebase console → Project settings → Your apps → Web app → "SDK setup and configuration" → Config
   firebase: {
